1dee5f8b1849be9aa014e84018e308fd6ac306cb
gusramirez-aplazo <gustavo@aplazo.mx>
2023-06-10 00:32:59 -0600

    chore: styles improvement

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-
+const track = document.getElementById('track');
 const slides = Array.from(document.querySelectorAll('.slider__item'));
 const backBtn = document.getElementById('back-btn');
 const fwdBtn = document.getElementById('fwd-btn');
-const slideSize = slides[0].getBoundingClientRect();
-const slideWidth = slideSize.width;
+const slideSize = slides[0].getBoundingClientRect().width;
 
 function moveFwd(e) {
-  const track = document.getElementById('track');
   const currentIndex = slides.findIndex((i) => i.classList.contains('current'));
   const next = slides[currentIndex].nextElementSibling;
 
-  track.style.setProperty("transform", `translateX(${slideSize * (currentIndex + 1)}px)`);
-  console.log(track)
-  slides[currentIndex].classList.remove('current');
-  next.classList.add('current');
+  if(next != null ){
+    track?.style.setProperty("transform", `translateX(-${slideSize * (currentIndex + 1)}px)`);
+    slides[currentIndex].classList.remove('current');
+    next?.classList.add('current');
+  }
 }
 
-fwdBtn.addEventListener('click', moveFwd);
+function moveBack(e) {
+  const currentIndex = slides.findIndex((i) => i.classList.contains('current'));
+  const prev = slides[currentIndex].previousElementSibling;;
+
+  if(prev != null ){
+    track?.style.setProperty("transform", `translateX(-${slideSize * (currentIndex - 1)}px)`);
+    slides[currentIndex].classList.remove('current');
+    prev?.classList.add('current');
+  }
+}
+
+fwdBtn?.addEventListener('click', moveFwd);
+backBtn?.addEventListener('click', moveBack);
